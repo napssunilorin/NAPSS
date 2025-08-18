3ba3be2ed0a58e8300c7187dbc927d3bc94fb438
napssunilorin <osenisamuel123@gmail.com>
2025-08-18 06:02:17 -0800

    hurfuybuipoi

--- .eleventy.js
+++ .eleventy.js
@@ -1,46 +1,31 @@
 const markdownIt = require("markdown-it");
 
 module.exports = function(eleventyConfig) {
-  // Pass through the images folder
-  eleventyConfig.addPassthroughCopy("images");
-  
-  // Pass through the admin folder
-  eleventyConfig.addPassthroughCopy("admin");
-
-  eleventyConfig.addFilter("markdown", function(value) {
-    let markdown = markdownIt();
-    return markdown.render(value);
-});
-
-  // Pass through other static files
-  eleventyConfig.addPassthroughCopy("NAPSS logo.jpg");
-  eleventyConfig.addPassthroughCopy("Departmental building.png");
-
-  // A custom collection for your news articles
-  eleventyConfig.addCollection("news", function(collection) {
-    return collection.getFilteredByGlob("_posts/*.md");
-  });
-
-  return {
-    dir: {
-      // Set the input directory to the project root
-      // This is where Eleventy will look for content and templates
-      input: "./",
-      
-      // Set the output directory to `_site`
-      // Netlify will serve the content from this folder
-      output: "_site"
-    }
-  };
-};
-
-module.exports = function(eleventyConfig) {
-    // Other configurations...
+    // Pass through the images folder. This copies the entire folder to _site.
+    eleventyConfig.addPassthroughCopy("images");
     
-    // This line tells Eleventy to copy the 'images' folder to the output directory.
-    eleventyConfig.addPassthroughCopy("img");
+    // Pass through the admin folder.
+    eleventyConfig.addPassthroughCopy("admin");
+
+    // Pass through individual image files from the root.
+    eleventyConfig.addPassthroughCopy("NAPSS logo.jpg");
+    eleventyConfig.addPassthroughCopy("Departmental building.png");
+
+    // Custom markdown filter to process markdown content in Liquid files.
+    eleventyConfig.addFilter("markdown", function(value) {
+        let markdown = new markdownIt();
+        return markdown.render(value);
+    });
+
+    // A custom collection for your news articles.
+    eleventyConfig.addCollection("news", function(collection) {
+        return collection.getFilteredByGlob("_posts/*.md");
+    });
 
     return {
-        // ...
+        // Set the input directory to the project root.
+        input: "./",
+        // Set the output directory to _site, where Netlify will serve the content.
+        output: "_site"
     };
 };
