fdabf2e811e5af9c9bbfeeaeb7588d693d5afd3e
napssunilorin <osenisamuel123@gmail.com>
2025-08-18 05:28:41 -0800

    eleventy edit

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,16 @@
+const markdownIt = require("markdown-it");
+
 module.exports = function(eleventyConfig) {
   // Pass through the images folder
   eleventyConfig.addPassthroughCopy("images");
   
   // Pass through the admin folder
   eleventyConfig.addPassthroughCopy("admin");
+
+  eleventyConfig.addFilter("markdown", function(value) {
+    let markdown = markdownIt();
+    return markdown.render(value);
+});
 
   // Pass through other static files
   eleventyConfig.addPassthroughCopy("NAPSS logo.jpg");
