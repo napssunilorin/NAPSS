05ad89c33c826ea86acc30d7e3ad4fd99dde8e35
napssunilorin <osenisamuel123@gmail.com>
2025-08-18 07:34:55 -0800

    TRSKJTJ

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,17 @@
 const markdownIt = require("markdown-it");
 
 module.exports = function(eleventyConfig) {
-    // This single line copies the entire `img` folder and all its contents to `_site/img`.
-    eleventyConfig.addPassthroughCopy("img");
+    // Pass through the images folder. This copies the entire folder to _site.
+    eleventyConfig.addPassthroughCopy("im");
     
     // Pass through the admin folder.
     eleventyConfig.addPassthroughCopy("admin");
 
-    // A custom markdown filter to process markdown content in Liquid files.
+    // Pass through individual image files from the root.
+    eleventyConfig.addPassthroughCopy("NAPSS logo.jpg");
+    eleventyConfig.addPassthroughCopy("Departmental building.png");
+
+    // Custom markdown filter to process markdown content in Liquid files.
     eleventyConfig.addFilter("markdown", function(value) {
         let markdown = new markdownIt();
         return markdown.render(value);
